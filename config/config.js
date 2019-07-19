1f0d90807be661c17aad827fb1d81d0aa5bca28e
cyberpepe <kekronet@gmail.com>
2019-07-19 02:18:48 -0400

    Default insight db in /home/USER/.kekdaq/insight for organization

--- config/config.js
+++ config/config.js
@@ -5,48 +5,48 @@ var fs = require('fs');
 var mkdirp = require('mkdirp');
 
 var rootPath = path.normalize(__dirname + '/..'),
-  env,
-  db,
-  port,
-  b_port,
-  p2p_port;
+    env,
+    db,
+    port,
+    b_port,
+    p2p_port;
 
 var packageStr = fs.readFileSync(rootPath + '/package.json');
 var version = JSON.parse(packageStr).version;
 
 
 function getUserHome() {
-  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+    return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
 
-var home = process.env.INSIGHT_DB || (getUserHome() + '/.insight');
+var home = process.env.INSIGHT_DB || (getUserHome() + '/.kekdaq/insight');
 
 if (process.env.INSIGHT_NETWORK === 'livenet') {
-  env = 'livenet';
-  db = home;
-  port = '3000';
-  b_port = '29376';
-  p2p_port = '29377';
+    env = 'livenet';
+    db = home;
+    port = '3000';
+    b_port = '29376';
+    p2p_port = '29377';
 } else {
-  env = 'testnet';
-  db = home + '/testnet';
-  port = '3001';
-  b_port = '18332';
-  p2p_port = '18333';
+    env = 'testnet';
+    db = home + '/testnet';
+    port = '3001';
+    b_port = '18332';
+    p2p_port = '18333';
 }
 port = parseInt(process.env.INSIGHT_PORT) || port;
 
 
 switch (process.env.NODE_ENV) {
-  case 'production':
-    env += '';
-    break;
-  case 'test':
-    env += ' - test environment';
-    break;
-  default:
-    env += ' - development';
-    break;
+    case 'production':
+        env += '';
+        break;
+    case 'test':
+        env += ' - test environment';
+        break;
+    default:
+        env += ' - development';
+        break;
 }
 
 var network = process.env.INSIGHT_NETWORK || 'testnet';
@@ -56,9 +56,9 @@ var isWin = /^win/.test(process.platform);
 var isMac = /^darwin/.test(process.platform);
 var isLinux = /^linux/.test(process.platform);
 if (!dataDir) {
-  if (isWin) dataDir = '%APPDATA%\\PepeCoin\\';
-  if (isMac) dataDir = process.env.HOME + '/Library/Application Support/PepeCoin/';
-  if (isLinux) dataDir = process.env.HOME + '/.pepecoin/';
+    if (isWin) dataDir = '%APPDATA%\\PepeCoin\\';
+    if (isMac) dataDir = process.env.HOME + '/Library/Application Support/PepeCoin/';
+    if (isLinux) dataDir = process.env.HOME + '/.pepecoin/';
 }
 dataDir += network === 'testnet' ? 'testnet3' : '';
 
@@ -67,16 +67,16 @@ var ignoreCache = process.env.INSIGHT_IGNORE_CACHE || 0;
 
 
 var bitcoindConf = {
-  protocol: process.env.BITCOIND_PROTO || 'http',
-  user: process.env.BITCOIND_USER || 'user',
-  pass: process.env.BITCOIND_PASS || 'pass',
-  host: process.env.BITCOIND_HOST || '127.0.0.1',
-  port: process.env.BITCOIND_PORT || b_port,
-  p2pPort: process.env.BITCOIND_P2P_PORT || p2p_port,
-  p2pHost: process.env.BITCOIND_P2P_HOST || process.env.BITCOIND_HOST || '127.0.0.1',
-  dataDir: dataDir,
-  // DO NOT CHANGE THIS!
-  disableAgent: true
+    protocol: process.env.BITCOIND_PROTO || 'http',
+    user: process.env.BITCOIND_USER || 'user',
+    pass: process.env.BITCOIND_PASS || 'pass',
+    host: process.env.BITCOIND_HOST || '127.0.0.1',
+    port: process.env.BITCOIND_PORT || b_port,
+    p2pPort: process.env.BITCOIND_P2P_PORT || p2p_port,
+    p2pHost: process.env.BITCOIND_P2P_HOST || process.env.BITCOIND_HOST || '127.0.0.1',
+    dataDir: dataDir,
+    // DO NOT CHANGE THIS!
+    disableAgent: true
 };
 
 var enableMonitor = process.env.ENABLE_MONITOR === 'true';
@@ -90,44 +90,44 @@ var loggerLevel = process.env.LOGGER_LEVEL || 'info';
 var enableHTTPS = process.env.ENABLE_HTTPS === 'true';
 
 if (!fs.existsSync(db)) {
-  mkdirp.sync(db);
+    mkdirp.sync(db);
 }
 
 module.exports = {
-  enableMonitor: enableMonitor,
-  monitor: require('../plugins/config-monitor.js'),
-  enableCleaner: enableCleaner,
-  cleaner: require('../plugins/config-cleaner.js'),
-  enableMailbox: enableMailbox,
-  mailbox: require('../plugins/config-mailbox.js'),
-  enableRatelimiter: enableRatelimiter,
-  ratelimiter: require('../plugins/config-ratelimiter.js'),
-  enableCredentialstore: enableCredentialstore,
-  credentialstore: require('../plugins/config-credentialstore'),
-  enableEmailstore: enableEmailstore,
-  emailstore: require('../plugins/config-emailstore'),
-  enablePublicInfo: enablePublicInfo,
-  publicInfo: require('../plugins/publicInfo/config'),
-  loggerLevel: loggerLevel,
-  enableHTTPS: enableHTTPS,
-  version: version,
-  root: rootPath,
-  publicPath: process.env.INSIGHT_PUBLIC_PATH || false,
-  appName: 'Insight ' + env,
-  apiPrefix: '/api',
-  port: port,
-  leveldb: db,
-  bitcoind: bitcoindConf,
-  network: network,
-  disableP2pSync: false,
-  disableHistoricSync: false,
-  poolMatchFile: rootPath + '/etc/minersPoolStrings.json',
+    enableMonitor: enableMonitor,
+    monitor: require('../plugins/config-monitor.js'),
+    enableCleaner: enableCleaner,
+    cleaner: require('../plugins/config-cleaner.js'),
+    enableMailbox: enableMailbox,
+    mailbox: require('../plugins/config-mailbox.js'),
+    enableRatelimiter: enableRatelimiter,
+    ratelimiter: require('../plugins/config-ratelimiter.js'),
+    enableCredentialstore: enableCredentialstore,
+    credentialstore: require('../plugins/config-credentialstore'),
+    enableEmailstore: enableEmailstore,
+    emailstore: require('../plugins/config-emailstore'),
+    enablePublicInfo: enablePublicInfo,
+    publicInfo: require('../plugins/publicInfo/config'),
+    loggerLevel: loggerLevel,
+    enableHTTPS: enableHTTPS,
+    version: version,
+    root: rootPath,
+    publicPath: process.env.INSIGHT_PUBLIC_PATH || false,
+    appName: 'Insight ' + env,
+    apiPrefix: '/api',
+    port: port,
+    leveldb: db,
+    bitcoind: bitcoindConf,
+    network: network,
+    disableP2pSync: false,
+    disableHistoricSync: false,
+    poolMatchFile: rootPath + '/etc/minersPoolStrings.json',
 
-  // Time to refresh the currency rate. In minutes
-  currencyRefresh: 10,
-  keys: {
-    segmentio: process.env.INSIGHT_SEGMENTIO_KEY
-  },
-  safeConfirmations: safeConfirmations, // PLEASE NOTE THAT *FULL RESYNC* IS NEEDED TO CHANGE safeConfirmations
-  ignoreCache: ignoreCache,
+    // Time to refresh the currency rate. In minutes
+    currencyRefresh: 10,
+    keys: {
+        segmentio: process.env.INSIGHT_SEGMENTIO_KEY
+    },
+    safeConfirmations: safeConfirmations, // PLEASE NOTE THAT *FULL RESYNC* IS NEEDED TO CHANGE safeConfirmations
+    ignoreCache: ignoreCache,
 };
