2760e37a91946c45eaa1b01587e5ff0242b5327b
cyberpepe <kekronet@gmail.com>
2019-07-25 16:17:23 -0400

    Bump heartbeat check to 20 seconds and reduce logspam

--- run.js
+++ run.js
@@ -1,42 +1,42 @@
 var fork = require('child_process').fork;
-var server, heartbeat; 
+var server, heartbeat;
 
-function startServer () {
-  console.log('Starting server');
-  server = fork('insight');
+function startServer() {
+    console.log('Starting server');
+    server = fork('insight');
 
-  //when the server goes down restart it
-  server.on('close', function(code) {
-    startServer();
-  });
+    //when the server goes down restart it
+    server.on('close', function(code) {
+        startServer();
+    });
 
-  //when server sends a heartbeat message save it
-  server.on('message', function(message) {
-    heartbeat = message ? message.heartbeat : null;
-  });
+    //when server sends a heartbeat message save it
+    server.on('message', function(message) {
+        heartbeat = message ? message.heartbeat : null;
+    });
 
-  //ask the server for a heartbeat
-  server.send({request: 'heartbeat'});
+    //ask the server for a heartbeat
+    server.send({ request: 'heartbeat' });
 
-  //wait 5 seconds and check if the server responded
-  setTimeout(checkHeartbeat, 5000);
+    //wait 5 seconds and check if the server responded
+    setTimeout(checkHeartbeat, 20000);
 }
 
 function checkHeartbeat() {
-  if(heartbeat) {
-    console.log('Server is alive');
+    if (heartbeat) {
+        // console.log('Server is alive');
 
-    //clear the heart beat and send request for a new one
-    heartbeat = null; 
-    server.send({request: 'heartbeat'});
+        //clear the heart beat and send request for a new one
+        heartbeat = null;
+        server.send({ request: 'heartbeat' });
 
-    //set another hearbeat check
-    setTimeout(checkHeartbeat, 5000);
+        //set another hearbeat check
+        setTimeout(checkHeartbeat, 20000);
 
-  } else {
-    console.log('Server looks stuck...killing');
-    server.kill();
-  }
+    } else {
+        console.log('Server looks stuck...killing');
+        server.kill();
+    }
 }
 
 startServer();
